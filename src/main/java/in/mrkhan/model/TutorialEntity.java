4b0335893a60af07a1f6c4f5dc139b45d03872c0
Nawed Khan <nawed@example.com>
2025-08-23 16:13:04 +0530

    Resolve conflicts: remove generated/IDE files and commit real code

--- src/main/java/in/mrkhan/model/TutorialEntity.java
+++ src/main/java/in/mrkhan/model/TutorialEntity.java
@@ -1,12 +1,10 @@
 package in.mrkhan.model;
 
-import javax.persistence.Column;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
 
 @Entity
 @Table(name = "TUTORIAL_TAB")
